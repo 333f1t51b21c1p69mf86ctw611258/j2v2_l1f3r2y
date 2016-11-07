f0c248ef7449d4108269dd38a1f5a9acabbf68af
nguyenth86 <mr.nguyenth86@gmail.com>
2016-11-08 00:40:00 +0700

    Fix language detect file

--- DNS/tms/tms-portlet-service/src/main/java/com/dasannetworks/vn/tms/service/BaseServiceInput.java
+++ DNS/tms/tms-portlet-service/src/main/java/com/dasannetworks/vn/tms/service/BaseServiceInput.java
@@ -2,59 +2,59 @@ package com.dasannetworks.vn.tms.service;
 
 public abstract class BaseServiceInput {
 
-	private boolean andSearchCondition;
-	private int start = -1;
-	private int end = -1;
-	private String sortField;
-	private String sortDirection;
-	private boolean exactly = false;
+    private boolean andSearchCondition;
+    private int start = -1;
+    private int end = -1;
+    private String sortField;
+    private String sortDirection;
+    private boolean exactly = false;
 
-	public boolean isAndSearchCondition() {
-		return andSearchCondition;
-	}
+    public boolean isAndSearchCondition() {
+        return andSearchCondition;
+    }
 
-	public void setAndSearchCondition(boolean andSearchCondition) {
-		this.andSearchCondition = andSearchCondition;
-	}
+    public void setAndSearchCondition(boolean andSearchCondition) {
+        this.andSearchCondition = andSearchCondition;
+    }
 
-	public int getStart() {
-		return start;
-	}
+    public int getStart() {
+        return start;
+    }
 
-	public void setStart(int start) {
-		this.start = start;
-	}
+    public void setStart(int start) {
+        this.start = start;
+    }
 
-	public int getEnd() {
-		return end;
-	}
+    public int getEnd() {
+        return end;
+    }
 
-	public void setEnd(int end) {
-		this.end = end;
-	}
+    public void setEnd(int end) {
+        this.end = end;
+    }
 
-	public String getSortField() {
-		return sortField;
-	}
+    public String getSortField() {
+        return sortField;
+    }
 
-	public void setSortField(String sortField) {
-		this.sortField = sortField;
-	}
+    public void setSortField(String sortField) {
+        this.sortField = sortField;
+    }
 
-	public String getSortDirection() {
-		return sortDirection;
-	}
+    public String getSortDirection() {
+        return sortDirection;
+    }
 
-	public void setSortDirection(String sortDirection) {
-		this.sortDirection = sortDirection;
-	}
+    public void setSortDirection(String sortDirection) {
+        this.sortDirection = sortDirection;
+    }
 
-	public boolean isExactly() {
-		return exactly;
-	}
+    public boolean isExactly() {
+        return exactly;
+    }
 
-	public void setExactly(boolean exactly) {
-		this.exactly = exactly;
-	}
+    public void setExactly(boolean exactly) {
+        this.exactly = exactly;
+    }
 
 }
--- DNS/tms/tms-portlet-service/src/main/java/com/dasannetworks/vn/tms/service/DeviceSearchInput.java
+++ DNS/tms/tms-portlet-service/src/main/java/com/dasannetworks/vn/tms/service/DeviceSearchInput.java
@@ -4,60 +4,60 @@ package com.dasannetworks.vn.tms.service;
  *
  */
 public class DeviceSearchInput extends BaseServiceInput {
-	private String serialNumber;
-	private String macAddress;
-	private String manufacturerSerial;
-	private String productName;
-	private String partNumber;
+    private String serialNumber;
+    private String macAddress;
+    private String manufacturerSerial;
+    private String productName;
+    private String partNumber;
 
-	public String getPartNumber() {
-		return partNumber;
-	}
+    public String getPartNumber() {
+        return partNumber;
+    }
 
-	public void setPartNumber(String partNumber) {
-		this.partNumber = partNumber;
-	}
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
+    }
 
-	public String getMacAddress() {
-		return macAddress;
-	}
+    public String getMacAddress() {
+        return macAddress;
+    }
 
-	public void setMacAddress(String macAddress) {
-		this.macAddress = macAddress;
-	}
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
 
-	public String getManufacturerSerial() {
-		return manufacturerSerial;
-	}
+    public String getManufacturerSerial() {
+        return manufacturerSerial;
+    }
 
-	public void setManufacturerSerial(String manufacturerSerial) {
-		this.manufacturerSerial = manufacturerSerial;
-	}
+    public void setManufacturerSerial(String manufacturerSerial) {
+        this.manufacturerSerial = manufacturerSerial;
+    }
 
-	public String getProductName() {
-		return productName;
-	}
+    public String getProductName() {
+        return productName;
+    }
 
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
 
-	public String getSerialNumber() {
-		return serialNumber;
-	}
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 
-	public void setSerialNumber(String serialNumber) {
-		this.serialNumber = serialNumber;
-	}
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 
-	public void advanceSearch(boolean advancedSearch, String keywords) {
-		if (!advancedSearch) {
-			setManufacturerSerial(keywords);
-			setProductName(keywords);
-			setAndSearchCondition(false);
-			setPartNumber(keywords);
-			setMacAddress(keywords);
-			setSerialNumber(keywords);
-		}
-	}
+    public void advanceSearch(boolean advancedSearch, String keywords) {
+        if (!advancedSearch) {
+            setManufacturerSerial(keywords);
+            setProductName(keywords);
+            setAndSearchCondition(false);
+            setPartNumber(keywords);
+            setMacAddress(keywords);
+            setSerialNumber(keywords);
+        }
+    }
 }
